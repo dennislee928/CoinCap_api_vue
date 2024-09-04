7cf25a8d840fb0a57c78d3225bda38af8cdf0a12
dennislee928 <55692615+dennislee928@users.noreply.github.com>
2024-09-04 17:13:32 +0800

    force push 2

---
+++ quasar-project/src/pages/Error404.vue
@@ -0,0 +1,26 @@
+<template>
+  <q-page class="flex flex-center">
+    <div class="text-center">
+      <q-icon name="error" size="5rem" color="negative" />
+      <div class="text-h4">404 - Page Not Found</div>
+      <q-btn color="primary" @click="goHome" label="Go Home" />
+    </div>
+  </q-page>
+</template>
+
+<script setup>
+import { useRouter } from "vue-router";
+
+// 將組件名稱更改為多字組合
+const componentName = "ErrorPage404";
+
+const router = useRouter();
+
+const goHome = () => {
+  router.push("/");
+};
+</script>
+
+<style scoped>
+/* Add your styles here */
+</style>
